5fa184552d5ef9562be003268334c13c371e37a8
Nicknakin <kinneyanick@gmail.com>
2018-08-23 19:42:08 -0500

    Minor text changes

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 const fs = require('fs');
-let csv = require('./list.csv');
+let csv = require('csv-file-creator');
 let continuousMsg = "";
 let interval;
 let counter = 0;
@@ -27,7 +27,6 @@ bot.on('message', (user, userID, channelID, message, evt) => {
     if(message.substring(0,2) == "s!"){
         let cmd = message.substring(2).split(' ');
         if(cmd[0] == "start"){
-            continuousMsg = "";
             recording = true;
             bot.sendMessage({
                     to: evt.d.channel_id,
@@ -39,8 +38,9 @@ bot.on('message', (user, userID, channelID, message, evt) => {
                     to: evt.d.channel_id,
                     message: continuousMsg
             });
-            continuousMsg = "";
+            console.log(continuousMsg);
             recording = false;
+            continuousMsg = "Pokémon,Level,Number,IV,Nickname\n";
         }
     } 
 });
@@ -49,7 +49,7 @@ bot.on('any', (evt) => {
     let author = (evt.d && evt.d.author)? evt.d.author: {username: ""};
     let username = (author.username)? author.username: "";
     let embeds = (evt.d && evt.d.embeds)? evt.d.embeds: "";
-    if(recording && username == "Pokécord" && embeds[0].title && embeds[0].title == "Your pokémon:"){
+    if(recording && embeds && username == "Pokécord" && embeds[0].title && embeds[0].title == "Your pokémon:"){
         let msg = (embeds[0].description);
         let list = msg.split('\n');
         for(let i = 0; i < list.length; i++){
@@ -61,13 +61,16 @@ bot.on('any', (evt) => {
             list[i][1] = list[i][1].substring(8);
             list[i][2] = list[i][2].substring(9);
             list[i][3] = list[i][3].substring(5);
+            if(list[i][4]){
+                list[i][4] = list[i][4].substring(10);
+            }
         }
         let newList = "";
         for(entry of list){
             for(item of entry){
                 newList += item.replace(' ', '') + ',';
             }
-            newList += '\n'; 
+            newList = newList.substring(0, newList.length-1) + '\n'; 
         }
         continuousMsg += newList;
     }
